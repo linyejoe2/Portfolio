a7a97e6e8b60c87a7c6cb2e108fd576dcb4ea5c9
RSP_linye <linyejoe2@gmail.com>
2024-01-11 16:58:13 +0800

    add tech tag to projects

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -36,11 +36,11 @@ export default function Home() {
           <br /><br />
           I'm using classics langauges like <HiLighter>TypeScript, C++</HiLighter> and <HiLighter>Python</HiLighter>.
           <br /><br />
-          I'm proficient in <HiLighter>Node.js, GIS</HiLighter> (Geographic Information Systems) and <HiLighter>QT Application</HiLighter>.
+          I'm proficient in <HiLighter>React, Vite, GIS</HiLighter> (Geographic Information Systems) and <HiLighter>QT Application</HiLighter>.
           <br /><br />
           I am eagerly seeking the next phase of my career in programming, where I can dedicate my efforts to the programming I'm passionate about.
           <br /><br />
-          If you happen to be looking for someone proficient in <HiLighter>TS, C++</HiLighter>, or if you need a talent to assist in developing <HiLighter>GIS or QT apps</HiLighter>, please feel free to <HiLighter>reach out</HiLighter> to me.
+          If you happen to be looking for someone proficient in <HiLighter>TypeScript, React</HiLighter>, or if you are looking for a <HiLighter>fast learner</HiLighter> and a <HiLighter>team player</HiLighter>, please feel free to <HiLighter>reach out</HiLighter> to me.
         </p>
       </Grid>
       <Grid xs={12} md={12}>
